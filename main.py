4d334c6e2390e1fbf72904ba1c6e0b12cd684fd8
SoofiiaBoinitska <sonia.boinitska@gmail.com>
2023-11-23 12:09:14 +0100

    #new 12:10

--- main.py
+++ main.py
@@ -445,8 +445,8 @@ def model_stats(train_true, test_true, result, model_name, model_type='ARIMA', e
 
         elif model_type == 'SARIMA':
             start_point = max(p + d + q, P + D + Q + s - 1)
-            #train_true_reduced = train_true[start_point:]
-            train_true_reduced = train_true_normalized[start_point:] #new 12:00
+            train_true_reduced = train_true[start_point:] #new 12:10
+            #train_true_reduced = train_true_normalized[start_point:] #new 12:00
 
             train_pred_normalized = result.predict(start=start_point, end=len(train_true) - 1, typ='levels')
             train_pred = minmax_rev(train_pred_normalized, train_true)
